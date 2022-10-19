f4119bc2071f3027b365a1d7b88021c7f00fda29
Fatumayattani <fyattani@gmail.com>
2022-10-19 11:48:42 -0700

    Printf

--- main.h
+++ main.h
@@ -2,6 +2,7 @@
 #define _MAIN_H_
 
 #include <stdarg.h>
+
 /**
 * struct print - print structure types
 * @t: type to print
@@ -12,19 +13,22 @@ typedef struct print
 {
 char *t;
 int (*f)(va_list);
-} print_t;
 
+} print_t;
 int _putchar(char c);
 int _printf(const char *format, ...);
-int print_function(va_list f);
-int print_functions1(va_list f1);
-int print_functions2(va_list f2);
-int print_flags(va_list fl);
-int print_precision(va_list p);
-int print_size(va_list s);
-int print_width(va_list w);
-int print_handle(va_list h);
-int print_utils(va_list u);
-int print_handlers(va_list hs);
+int print_c(va_list c);
+int print_s(va_list s);
+int print_i(va_list i);
+int print_d(va_list d);
+int print_u(va_list u);
+int print_b(va_list b);
+int print_o(va_list o);
+int print_x(va_list x);
+int print_X(va_list X);
+int print_p(va_list p);
+int print_S(va_list S);
+int print_r(va_list r);
+int print_R(va_list R);
 
 #endif  /* _MAIN_H */
